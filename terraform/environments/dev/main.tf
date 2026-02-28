0a233b321fa78287e46540b97e599accdc34cf29
jili14edl <80779024+jili14edl@users.noreply.github.com>
2026-02-28 15:42:21 +0530

    Update main.tf

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,11 +1,18 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = var.aws_region
 }
 
-module "dev_eks" {
-  source       = "../../modules/eks_cluster"
-  env_name     = "dev"
-  vpc_id       = "vpc-04670c78280cef62c" # Your specific VPC
-  desired_size = 1
-  max_size     = 2
+module "eks_deploy" {
+  source        = "../../modules/eks_cluster"
+  vpc_id        = var.vpc_id
+  env_name      = var.env_name
+  desired_nodes = var.desired_nodes
+  max_nodes     = var.max_nodes
 }
+
+# Define these locally so they can be fed into the module
+variable "aws_region"    {}
+variable "vpc_id"        {}
+variable "env_name"      {}
+variable "desired_nodes" {}
+variable "max_nodes"     {}
