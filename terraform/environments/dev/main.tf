a2eb74223b207fcda1066e87e1395caa7b312e1e
jili14edl <jili14edl@gmail.com>
2026-02-28 18:40:38 +0530

    terraform

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -4,15 +4,29 @@ provider "aws" {
 
 module "eks_deploy" {
   source        = "../../modules/eks_cluster"
-  vpc_id        = var.vpc_id
-  env_name      = var.env_name
-  desired_nodes = var.desired_nodes
-  max_nodes     = var.max_nodes
+  vpc_id             = var.vpc_id
+  env_name           = var.env_name
+  desired_nodes      = var.desired_nodes
+  max_nodes          = var.max_nodes
+  private_subnet_ids = module.private_subnets.private_subnet_ids
+}
+
+module "private_subnets" {
+  source = "../../modules/subnets"
+
+  vpc_id               = var.vpc_id
+  env_name             = var.env_name
+  availability_zones   = var.availability_zones
+  private_subnet_cidrs = var.private_subnet_cidrs
+  tags                 = var.default_tags
 }
 
 # Define these locally so they can be fed into the module
-variable "aws_region"    {}
-variable "vpc_id"        {}
-variable "env_name"      {}
-variable "desired_nodes" {}
-variable "max_nodes"     {}
+variable "aws_region"        {}
+variable "vpc_id"            {}
+variable "env_name"          {}
+variable "desired_nodes"     {}
+variable "max_nodes"         {}
+variable "availability_zones"   {}
+variable "private_subnet_cidrs" {}
+variable "default_tags"         { default = {} }
--- terraform/environments/dev/terraform.tfvars
+++ terraform/environments/dev/terraform.tfvars
@@ -3,3 +3,14 @@ vpc_id        = "vpc-04670c78280cef62c"
 env_name      = "dev"
 desired_nodes = 1
 max_nodes     = 2
+
+availability_zones = ["ap-south-1a", "ap-south-1b"]
+private_subnet_cidrs = [
+  "10.0.1.0/24",
+  "10.0.2.0/24"
+]
+
+default_tags = {
+  Owner = "devops"
+  Env   = "dev"
+}
